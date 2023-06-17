324c69569509c02836cf7eb0098c894ae2a2c952
mohammadyousefi02 <mohammadmehdiyousefi02@gmail.com>
2023-06-17 23:17:49 +0330

    add new methods to user service

--- src/main/kotlin/com/medical/medicalshop/user/UserService.kt
+++ src/main/kotlin/com/medical/medicalshop/user/UserService.kt
@@ -1,10 +1,31 @@
 package com.medical.medicalshop.user
 
+import com.medical.medicalshop.auth.SignupDto
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
-class UserService(private val userRepository: UserRepository) {
-    fun getAllUsers():List<User> {
-        return userRepository.findAll();
+class UserService(private val userRepository: UserRepository, private val passwordEncoder: PasswordEncoder) {
+    fun getAllUsers(): List<User> {
+        return userRepository.findAll()
+    }
+
+    fun findById(id: Long): Optional<User> {
+        return userRepository.findById(id)
+    }
+
+    fun findByEmail(email: String): Optional<User> {
+        return userRepository.findByEmail(email)
+    }
+
+    fun signup(signupDto: SignupDto): User? {
+        val user = findByEmail(signupDto.email)
+        if (user.isPresent) return null
+        val newUser = User()
+        newUser.username = signupDto.username
+        newUser.email = signupDto.email
+        newUser.password = passwordEncoder.encode(signupDto.password)
+        return userRepository.save(newUser)
     }
 }
