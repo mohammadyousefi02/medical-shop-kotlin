b7e0d9207f6794a9abf8b26085b5b92e7373363b
mohammadyousefi02 <mohammadmehdiyousefi02@gmail.com>
2023-06-18 04:06:23 +0330

    change product dto

---
+++ src/main/kotlin/com/medical/medicalshop/product/ProductDto.kt
@@ -0,0 +1,13 @@
+package com.medical.medicalshop.product
+
+class ProductDto {
+    var id: Long? = null
+    var title: String? = null
+    var description: String? = null
+    var price: Long? = null
+    var stock = 0
+    var madein: String? = null
+    var company: String? = null
+    var image: String? = null
+    var category: Long? = null
+}
